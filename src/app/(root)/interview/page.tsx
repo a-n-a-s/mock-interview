df9d34fe0f4a346b5073ef2942831a76ab427466
a-n-a-s <mirzaanas815@gmail.com>
2025-04-14 19:56:14 +0530

    3

---
+++ src/app/(root)/interview/page.tsx
@@ -0,0 +1,12 @@
+import Agentai from '@/components/Agentai'
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+        <Agentai  userName="you" userId="user1"  type="generate" />
+    </>
+  )
+}
+
+export default page
